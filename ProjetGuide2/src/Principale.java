fd406e2f2b98fb894b2066904ea36401e01d75b5
H4KRO <32486404+H4KRO@users.noreply.github.com>
2020-01-31 17:42:39 +0100

    Clean

--- ProjetGuide2/src/Principale.java
+++ ProjetGuide2/src/Principale.java
@@ -7,7 +7,7 @@ public class Principale {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		Socket socket = new Socket("192.168.100.25", 5999);
 		IOCommandes ioc = new IOCommandes(socket);
-		String msg = "";
+		String msg;
 		do {
 			msg = ioc.lireEcran();
 			ioc.ecrireReseau(msg);
